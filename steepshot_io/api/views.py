feaa37872142e4846349d94d65a130c05b4ccce1
Dmitry Yushko <yushko07@gmail.com>
2018-10-22 17:54:57 +0300

    Adding new view

--- steepshot_io/api/views.py
+++ steepshot_io/api/views.py
@@ -5,3 +5,7 @@ from steepshot_io.api import serializers
 
 class WorkRequestAPIView(CreateAPIView):
     serializer_class = serializers.WorkRequestSerializer
+
+
+class RequestAPIView(WorkRequestAPIView):
+    serializer_class = serializers.RequestSerializer
